0ecacec47c2afc85e22543aeb93e380accba9d29
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-10-23 14:41:57 +0200

    Switch dataprep script to be more extensible

--- dataprep.py
+++ dataprep.py
@@ -2,35 +2,49 @@ import gzip
 import os
 import pickle
 
+import click
 import wget
 from rdkit import Chem, RDLogger, rdBase
 from tqdm import tqdm
 
-print('RDKit Version', rdBase.rdkitVersion)
 RDLogger.DisableLog("rdApp.warning")
 
-url = 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/latest/chembl_27.sdf.gz'
-sdf_path = './chembl_27.sdf.gz'
-sss_path = './data/chembl27_sssdata.pkl'
 
-if not os.path.exists(sdf_path):
-    filename = wget.download(url)
+@click.command()
+@click.option('--directory', type=click.Path(file_okay=False, dir_okay=True), default=os.getcwd)
+@click.option('--chebml-version', default='27', show_default=True)
+def main(directory: str, chebml_version: str):
+    """Download the ChEBML data."""
+    url = (
+        f'ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/'
+        f'chembl_{chebml_version}/chembl_{chebml_version}.sdf.gz'
+    )
+    os.makedirs(directory, exist_ok=True)
 
-os.makedirs('./data', exist_ok=True)
+    sdf_path = os.path.join(directory, f'chembl_{chebml_version}.sdf.gz')
+    if not os.path.exists(sdf_path):
+        wget.download(url, out=directory)
 
-if not os.path.exists(sss_path):
-    data = []
+    sss_path = os.path.join(directory, f'chembl{chebml_version}_sssdata.pkl')
+    if not os.path.exists(sss_path):
+        click.echo(f'RDKit Version: {rdBase.rdkitVersion}')
+        data = []
 
-    with gzip.GzipFile(sdf_path) as gz:
-        suppl = Chem.ForwardSDMolSupplier(gz)
-        for mol in tqdm(suppl, desc='Processing molecules', unit_scale=True):
-            if mol is None or mol.GetNumAtoms() > 50:
-                continue
-            fp = Chem.PatternFingerprint(mol)
-            smi = Chem.MolToSmiles(mol)
-            data.append((smi, fp))
+        with gzip.GzipFile(sdf_path) as gz:
+            suppl = Chem.ForwardSDMolSupplier(gz)
+            for mol in tqdm(suppl, desc=f'Processing ChEBML {chebml_version}', unit_scale=True):
+                if mol is None or mol.GetNumAtoms() > 50:
+                    continue
+                fp = Chem.PatternFingerprint(mol)
+                smi = Chem.MolToSmiles(mol)
+                data.append((smi, fp))
 
-    with open(sss_path, 'wb') as file:
-        pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
+        click.echo(f'Outputting to {sss_path}')
+        with open(sss_path, 'wb') as file:
+            pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
 
-print('Done ;)')
+    click.echo('Done ;)')
+
+
+if __name__ == '__main__':
+    main()
